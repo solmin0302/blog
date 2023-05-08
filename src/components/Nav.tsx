8186ab81e274653805a66583e16c52fc7e77635e
solmin <solgo123@gmail.com>
2023-05-08 16:02:05 +0900

    :lipstick: Style update nav (GNB)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,26 @@
+"use client";
 import { navLinks } from "@/data/navLinks";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
+  const curPath = usePathname();
+
+  const defaultLinkStyle = `mr-5 text-lg transition hover:scale-125 hover:text-violet-500 duration-250`;
+
   return (
     <nav>
       {navLinks.map(({ title, link }) => {
         return (
-          <Link className="mr-5" key={title} href={link}>
+          <Link
+            className={
+              curPath === link
+                ? defaultLinkStyle + " text-violet-500"
+                : defaultLinkStyle
+            }
+            key={title}
+            href={link}
+          >
             {title}
           </Link>
         );
