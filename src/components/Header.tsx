1f2a04725f84fa872e7644195e0c802ffc4d67c3
solmin <solgo123@gmail.com>
2023-05-10 19:24:23 +0900

    :lipstick: Update main logo / header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/assets/img/dummyImage.png";
+import logo from "@/assets/img/logo.png";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,8 +12,8 @@ export default function Header() {
       </Head>
       <header className="sticky top-0 left-0 w-full z-10 h-20 bg-white dark:bg-[#111111] shadow-[0_5px_7px_0px_#ececec]">
         <div className="flex flex-nowrap max-w-screen-md h-20 items-center justify-between m-auto px-8">
-          <Link href="/" className="w-180 h-30">
-            <Image src={Logo} alt="blogLogo" />
+          <Link href="/" className="w-1/4">
+            <Image src={logo} alt="blogLogo" />
           </Link>
           <Nav />
         </div>
